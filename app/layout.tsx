899b25ad7bb6c96b46e5079cc51c32aab268d84f
Fidel Ernesto <fidehlg89@gmail.com>
2024-03-08 21:18:14 -0500

    chore: layout adjusts

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,19 +7,17 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Gateways app",
-  description: "Gateways app"
+  description: "Gateways app",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
